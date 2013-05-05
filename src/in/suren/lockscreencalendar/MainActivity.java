c8f89d4838c0797de55fe6d883b5723a78a31404
酥仁 <suren1986@gmail.com>
2013-05-05 16:34:09 +0800

    添加底部按钮

--- src/in/suren/lockscreencalendar/MainActivity.java
+++ src/in/suren/lockscreencalendar/MainActivity.java
@@ -2,7 +2,9 @@ package in.suren.lockscreencalendar;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
 
@@ -10,13 +12,32 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.Main).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                View optionsBar = (View) findViewById(R.id.OptionsBar);
+                if (optionsBar.getVisibility() != View.VISIBLE) {
+                    optionsBar.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        findViewById(R.id.btn_preview).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.OptionsBar).setVisibility(View.INVISIBLE);
+            }
+        });
     }
 
-    // @Override
-    // public boolean onCreateOptionsMenu(Menu menu) {
-    // // Inflate the menu; this adds items to the action bar if it is present.
-    // getMenuInflater().inflate(R.menu.main, menu);
-    // return true;
-    // }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
 }
